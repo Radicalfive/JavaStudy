a31b520fd2b08c63d449352e62f582035d1df6e8
rafical <1561548288@qq.com>
2021-10-15 22:19:03 +0800

    Day 23 浅拷贝or 深拷贝

--- java-basic/src/main/java/com/wx/java/basic/oop/Cloneable.java
+++ java-basic/src/main/java/com/wx/java/basic/oop/Cloneable.java
@@ -12,21 +12,22 @@ class Address implements Cloneable{
     }
 }
 @Data
-class Student implements Cloneable {
+class Student implements Cloneable{
     private Integer id;
     private String name;
     private Address address;
 
-    //@Override
-    //protected Object clone() throws CloneNotSupportedException {
-    //    //浅拷贝
-    //    return super.clone();
-    //}
     @Override
     protected Object clone() throws CloneNotSupportedException {
         //浅拷贝
-        Student s = (Student) super.clone();
-        s.address = (Address) this.address.clone();
-        return s;
+        return super.clone();
+
     }
+    // @Override
+    // protected Object clone() throws CloneNotSupportedException {
+    //    //浅拷贝
+    //    Student s = (Student) super.clone();
+    //    s.address = (Address) this.address.clone();
+    //    return s;
+    //}
 }
