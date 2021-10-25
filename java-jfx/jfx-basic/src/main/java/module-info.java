3c9c951e9ea512c43f5ab6190c14fb543caf02d5
rafical <1561548288@qq.com>
2021-10-25 23:28:44 +0800

    Day33 系统托盘&弹窗

--- java-jfx/jfx-basic/src/main/java/module-info.java
+++ java-jfx/jfx-basic/src/main/java/module-info.java
@@ -9,6 +9,10 @@ module com.radical.jfxbasic {
     exports com.radical.jfxbasic.chart;
     opens com.radical.jfxbasic.chart to javafx.fxml;
 
+    exports com.radical.jfxbasic.trayicon;
+    opens com.radical.jfxbasic.trayicon to javafx.fxml;
+
     requires com.github.oshi;
     requires fastjson;
+    requires java.desktop;
 }
