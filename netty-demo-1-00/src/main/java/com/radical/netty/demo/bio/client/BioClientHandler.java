6a8839a646a3ecb551968cd184a45c4dae12aceb
rafical <1561548288@qq.com>
2021-10-30 23:34:34 +0800

    Day38 netty-demo服务端

---
+++ netty-demo-1-00/src/main/java/com/radical/netty/demo/bio/client/BioClientHandler.java
@@ -0,0 +1,33 @@
+package com.radical.netty.demo.bio.client;
+
+import com.radical.netty.demo.bio.ChannelAdapter;
+import com.radical.netty.demo.bio.ChannelHandler;
+
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @description: BIO客户端消息处理器
+ * @author: radical
+ * @date: 2021/10/30
+ */
+public class BioClientHandler extends ChannelAdapter {
+
+    public BioClientHandler(Socket socket, Charset charset) {
+        super(socket, charset);
+    }
+
+    @Override
+    public void channelActive(ChannelHandler ctx) {
+        System.out.println("LocalAddress:" + ctx.socket().getLocalAddress());
+        ctx.writeAndFlush("我是BIO客户端的消息\r\n");
+    }
+
+    @Override
+    public void channelRead(ChannelHandler ctx, Object msg) {
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " 接收到消息：" + msg);
+        ctx.writeAndFlush("hi 我已经收到你的消息！\r\n");
+    }
+}
